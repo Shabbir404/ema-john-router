9ec5474bf0b46a2a72daf2b80751895416ae3cfd
Shabbir Ahmed <shabbirkimgbrand55@gmail.com>
2021-10-01 08:38:15 +0600

    done

--- src/components/Invantory/Invantory.js
+++ src/components/Invantory/Invantory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Invantory = () => {
     return (
         <div>
-            <h3> this is invantory</h3>
+            <h3> this is invantory page</h3>
         </div>
     );
 };
