994f0d4640626888745d709c90311ce2678d7264
Shabbir Ahmed <shabbirkimgbrand55@gmail.com>
2021-10-01 08:35:09 +0600

    fisrt-commmit

---
+++ src/components/Invantory/Invantory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Invantory = () => {
+    return (
+        <div>
+            <h3> this is invantory</h3>
+        </div>
+    );
+};
+
+export default Invantory;
